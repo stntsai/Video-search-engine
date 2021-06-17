858db815eda0118a5eb43f6a6065d3c831b2334c
Steven <stntsai93@gmail.com>
2021-06-17 14:27:51 +0800

    video list and video item component with youtube api

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+
+class App extends Component {
+    render(){
+        return (
+            <div className="ui container">
+                <SearchBar/>
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    state ={term:''};
+
+    onInputChange=(event)=>{
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit=(event)=>{
+        event.preventDefault();
+        
+    }
+
+    render(){
+        return (
+            <div className="search-bar ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field">
+                        <label> video search</label>
+                        <input type="text" value = {this.state.term} onChange={this.onInputChange}/>
+                    </div>
+                </form>
+            </div>
+        ) 
+    }
+}
+
+export default SearchBar;
